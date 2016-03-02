7a86646031e09b1050cd55d067b19fc582b748c0
Pierrick <pierrick.varin@zagett.com>
2016-03-02 02:58:02 +0100

    Added custom events and .on() method to bind listeners. Also some quick css

--- js/Synth.js
+++ js/Synth.js
@@ -14,6 +14,14 @@ var Synth = function() {
 	this.VCA;
 	this.MasterOutput;
 
+	this.listeners = {
+		__noteOn: null,
+		__noteOff: null,
+		__chordOn: null,
+		__chordOff: null,
+		__chordPlayed: null
+	}
+
 	this.boot = function() {
 		// Init VCA Stage
 		this.VCA = this.context.createGain();
@@ -28,6 +36,11 @@ var Synth = function() {
 	// Boot
 	this.boot();
 
+	// Methods
+	this.on = function(ev, cb) {
+		this.listeners['__' + ev] = cb;
+	}
+
 	this.noteOn = function(noteName) {
 		// Stops previous osc if voice was already in use
 		if( this.currentVoices[noteName] ) {
@@ -38,7 +51,7 @@ var Synth = function() {
 		var voiceVCO = this.context.createOscillator();
 		voiceVCO.type = this.oscType;
 		voiceVCO.frequency.value = Harmony.noteToFreq(noteName);
-		console.log('=> ', Harmony.noteToFreq(noteName));
+		
 		// Connect VCO to VCA Stage
 		voiceVCO.connect(this.VCA);
 
@@ -48,11 +61,21 @@ var Synth = function() {
 		// Play !
 		voiceVCO.start(0);
 
+		// Trigger listener
+		if( this.listeners.__noteOn ) {
+			this.listeners.__noteOn(noteName);
+		}
+
 		return noteName;
 	}
 
 	this.noteOff = function(noteName) {
 		var osc = this.currentVoices[noteName];
+
+		// Trigger listener
+		if( this.listeners.__noteOff ) {
+			this.listeners.__noteOff(noteName);
+		}
 
 		if( ! osc ) {
 			return;
@@ -64,9 +87,14 @@ var Synth = function() {
 
 	this.chordOn = function(chordName, octave) {
 		var chord = Harmony.chordToNotes(chordName, octave);
-		console.log('Playing '+chordName, chord);
 
 		if( chord.length ) {
+
+			// Trigger listener
+			if( this.listeners.__chordOn ) {
+				this.listeners.__chordOn(chordName, octave);
+			}
+
 			for(var i in chord) {
 				this.noteOn(chord[i]);
 			}
@@ -77,6 +105,11 @@ var Synth = function() {
 
 	this.chordOff = function(chordName, octave) {
 		var chord = this.currentChords[chordName+""+octave];
+
+		// Trigger listener
+		if( this.listeners.__chordOff ) {
+			this.listeners.__chordOff(chordName, octave);
+		}
 
 		if( ! chord.length ) {
 			return;
@@ -107,6 +140,10 @@ var Synth = function() {
 				durationMs = listOfChords[chordIndex][2];
 
 			that.chordOn(chordName, octave);
+
+			if( that.listeners.__chordPlayed ) {
+				that.listeners.__chordPlayed(chordName, octave, durationMs);
+			}
 
 			setTimeout( function() {
 				that.chordOff(chordName, octave);
